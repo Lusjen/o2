1928af8f95870471d74738044d37234ebfab5891
Andrew Basiuk <a.basyuk4779@gmail.com>
2018-07-09 12:06:51 +0300

    added our-contacts block

--- dist/includes/contacts.php
+++ dist/includes/contacts.php
@@ -10,6 +10,25 @@
             <h3 class="heading contacts__heading">
                 Наші контактні дані
             </h3>
+            <ul class="our-contact-list">
+                <li class="our-contact-list__item our-contact-list__item_1">
+                    <p class="our-contact-list__heading">Наш телефон:</p>
+                    <p class="our-contact-list__text">+380 44 364 50 51</p>
+                </li>
+                <li class="our-contact-list__item our-contact-list__item_2">
+                    <p class="our-contact-list__heading">Наша адреса:</p>
+                    <p class="our-contact-list__text">Київ, вулиця Льва Толстого, будинок 57</p>
+                </li>
+                <li class="our-contact-list__item our-contact-list__item_3">
+                    <p class="our-contact-list__heading">Наш e-mail:</p>
+                    <p class="our-contact-list__text">info@saga-development.com.ua</p>
+                </li>
+                <li class="our-contact-list__item our-contact-list__item_4">
+                    <span class="our-contact-list__heading our-contact-list__heading_margin-right">Години роботи відділу продажів:</span>
+                    <span class="our-contact-list__text our-contact-list__text_margin-right">Пн-пт: 09.00 - 19.00</span>
+                    <span class="our-contact-list__text">Сб-нд: 10.00 - 18.00</span>
+                </li>
+            </ul>
         </div>
         
         <div id="get-callback" class="contacts__tab-content contacts__tab-content-2">
